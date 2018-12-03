8eb28639ce03f79b55e9376b18b267b7886d15fb
Luis Perez Jimenez <luis.perez.jimenez@avanade.com>
2018-12-03 12:12:21 +0100

    Add project files.

---
+++ Palyndrom/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Palyndrom
+{
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadenaToCheck = "Never odd or even!";
+            
+            bool bOk = IsPalyndrom(cadenaToCheck);
+            Console.WriteLine($"{cadenaToCheck}:{bOk}");
+        }
+
+        static bool IsPalyndrom(string cadena)
+        {
+            string pattern = @"(\s+|@|&|'|\(|\)|<|>|#|!)";
+            cadena = Regex.Replace(cadena.ToLower(), pattern, "");
+
+            if (cadena.Length < 2) return true;
+
+            if (cadena[0].Equals(cadena[cadena.Length - 1]))
+            {
+                return IsPalyndrom(cadena.Substring(1, cadena.Length - 2));
+            }
+
+            return false;
+        }
+    }
+}
